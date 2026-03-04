a006e76751d8b69d14666287e4bea74eec6622ce
KanagawaYamada <albert.wesley.dion@gmail.com>
2026-03-04 21:18:03 +0700

    A dirty fix of Multiple instance of daemon
    Signed-off-by: KanagawaYamada <albert.wesley.dion@gmail.com>

--- Modules/service.sh
+++ Modules/service.sh
@@ -6,4 +6,7 @@ while [ -z "$(getprop sys.boot_completed)" ]; do
     sleep 10
 done
 
-su -c $MODDIR/Binaries/TenebrionDaemon_arm64 &
+# Dirty Fix, Prevent Daemon to run twice -_- [DO NOT REMOVE FOR NOW]
+killall TenebrionDaemon_arm64 2>/dev/null
+
+$MODDIR/Binaries/TenebrionDaemon_arm64 &
